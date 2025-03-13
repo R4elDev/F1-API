94f36bef42c16ae27212ea1427597d9b14751e5d
Pedrohenrique1309 <pedrohenrique.jorge@outlook.com>
2025-03-13 12:26:53 -0300

    adicionando a terceira tela

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-
 function mostrarTela(tela) {
     // Esconde todas as telas
     document.getElementById("telaInicial").style.display = "none";
     document.getElementById("telaHome").style.display = "none";
-  
+    document.getElementById("dadosPiloto").style.display = "none";
+    
     // Mostra a tela escolhida
     if (tela === 'inicial') {
       document.getElementById("telaInicial").style.display = "block";
     } else if (tela === 'home') {
       document.getElementById("telaHome").style.display = "block";
+    } else if (tela === 'dadosPiloto') {
+      document.getElementById("dadosPiloto").style.display = "block";
     }
   }
