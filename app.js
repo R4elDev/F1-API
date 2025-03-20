e58d7465510dbac1462d5c15b86e3dceae16a244
Pedrohenrique1309 <pedrohenrique.jorge@outlook.com>
2025-03-20 08:51:30 -0300

    finalizando layout das telas e adicionando js

--- app.js
+++ app.js
@@ -1,15 +1,84 @@
-function mostrarTela(tela) {
-    // Esconde todas as telas
-    document.getElementById("telaInicial").style.display = "none";
-    document.getElementById("telaHome").style.display = "none";
-    document.getElementById("dadosPiloto").style.display = "none";
+//Link API F1
+const apiF1 = "https://ergast.com/api/f1/constructors.json"
+
+ // Faz o Js esperar o carregamento do html
+ document.addEventListener('DOMContentLoaded', function() {
+
+//Função tela inicial
+async function telaInicial() {
     
-    // Mostra a tela escolhida
-    if (tela === 'inicial') {
-      document.getElementById("telaInicial").style.display = "block";
-    } else if (tela === 'home') {
-      document.getElementById("telaHome").style.display = "block";
-    } else if (tela === 'dadosPiloto') {
-      document.getElementById("dadosPiloto").style.display = "block";
-    }
-  }
+  // Input de pesquisa da tela inicial
+  const inputPesquisa = document.getElementById('pesquisarEquipe');
+
+   // Detecta ao clicar enter no input
+  inputPesquisa.addEventListener('keydown', function(event) {
+
+      if (event.key === 'Enter') {
+          
+        //Pega o valor digitado no input
+        let nomeEquipe = inputPesquisa.value.trim();
+ 
+        // Verifica se o nome da equipe foi informado
+        if (nomeEquipe === "") {
+          alert("Digite uma equipe!");
+          return;
+        }
+        
+        //Muda para a tela home  
+        window.location.href = 'telaHome.html';
+
+        buscarPiloto()
+
+       }
+   });
+
+}
+
+//Função tela home
+async function telaHome(){
+
+  // Verifica se o botão não teve erro ao executar
+  if (botaoCard) {
+    // Adiciona o click no botão
+    botaoCard.addEventListener("click", function() {
+        //Muda para a tela de dados dos pilotos
+        window.location.href = "telaDadosPilotos.html";
+    });
+  } else {
+    console.error("Botão não encontrado!");
+  };
+
+}
+
+//Função para buscar piloto
+async function buscarPiloto() {
+         
+
+}
+
+//Função para obter a foto do piloto na API OPEN F1
+async function obterFotoPiloto() {
+
+  
+}
+
+// Função para criar um card de piloto
+function criarCardPiloto() {
+
+}
+
+async function telaDadosPilotos(){
+  
+  
+
+}
+
+telaInicial()
+
+telaHome()
+
+obterFotoPiloto()
+
+criarCardPiloto()
+
+})
