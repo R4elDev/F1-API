917d3cdaabaeed941c1af2eff6ce82ec3087360a
Pedrohenrique1309 <pedrohenrique.jorge@outlook.com>
2025-03-21 10:34:33 -0300

    testando data

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  telaInicial(); // Chame a função que precisa acessar os elementos
+});
+
 //Função tela inicial
 async function telaInicial() {
     
@@ -26,108 +30,46 @@ async function telaInicial() {
        }
    });
 
-   console.log(nomeEquipe)
 
-  return nomeEquipe
+  // return nomeEquipe
 
 }
 
 //Função tela home
-async function telaHome(pilotoEncontrado){
+// async function telaHome(pilotoEncontrado){
 
-  // Verifica se o botão não teve erro ao executar
-  if (botaoCard) {
-    // Adiciona o click no botão
-    botaoCard.addEventListener("click", function() {
-        //Muda para a tela de dados dos pilotos
-        window.location.href = "telaDadosPilotos.html";
-    });
-  } else {
-    console.error("Botão não encontrado!");
-  };
-
-    /*const botaoCard = document.createElement("button")
-    botaoCard.className = 'botaoCard'
-
-    const pilotosContainer = document.createElement('div')
-    pilotosContainer.className = 'pilotosContainer'
-
-    const retangulo = document.createElement('div')
-    retangulo.className = 'retangulo'
-
-    const nomePiloto = document.createElement('h1')
-    nomePiloto.className = 'nomePiloto'
-
-    const fotoPiloto = document.createElement('div')
-    fotoPiloto.className = 'fotoPiloto'
-
-    const img = document.createElement('img')
-
-    const novoItem = document.createElement('li')
-
-
-    //adicionando conteúdo na tag
-    nomePiloto.textContent = pilotoEncontrado.nome
-    img.src = `./${pilotoEncontrado.foto}`
-
-    novoItem.style = `--cor-hover: ${pilotoEncontrado.corEquipe}`
-
-
-     //adicionando no pai
-     botaoCard.appendChild(pilotosContainer)
-     pilotosContainer.appendChild(retangulo)
-     pilotosContainer.appendChild(nomePiloto)
-     pilotosContainer.appendChild(fotoPiloto)
-     fotoPiloto.appendChild(img)*/
+//   // Verifica se o botão não teve erro ao executar
+//   if (botaoCard) {
+//     // Adiciona o click no botão
+//     botaoCard.addEventListener("click", function() {
+//         //Muda para a tela de dados dos pilotos
+//         window.location.href = "telaDadosPilotos.html";
+//     });
+//   } else {
+//     console.error("Botão não encontrado!");
+//   };
 
   
 
 
-}
-
-//Função para buscar piloto
-/*async function buscarPiloto(nomeEquipe) {
-
-  console.log(nomeEquipe)
-        
-  if (nomeEquipe === "") {
-      alert("Digite uma equipe!");
-      return alert;
-  }
-
-      // Realiza a requisição para buscar pilotos da equipe
-      const url1 = `https://ergast.com/api/f1/constructors/${nomeEquipe}/drivers.json`;
-      const response1 = await fetch(url1);
-      const data1 = await response1.json();
-      const pilotos = data1.MRData.DriverTable.Drivers;
-      const numeroPilotoEcontrado = []
-
-      console.log(response1)
-
-      dataDesc.forEach(function(item){
-        numeroPilotoEcontrado.push(item['permanentNumber'])
-      })
-
-      if (!pilotos || pilotos.length === 0) {
-          alert("Nenhuma equipe encontrada ou sem pilotos.");
-          return alert;
-      }
-
-    return numeroPilotoEcontrado
-  
-
-}*/
+// }
 
 async function getBuscarPiloto(){
   // Realiza a requisição para buscar pilotos da equipe
   const url = `https://api.openf1.org/v1/drivers`;
   const response = await fetch(url);
   const data = await response.json();
-  const dataDesc =  await data.description
+  // Os objetos chegam aqui
+  //console.log(data);
+
+  return data
+  
 }
 
 //Função para buscar piloto
 async function buscarPiloto(nomeEquipe) {
+
+  //let teste = "Red Bull Racing"
 
   //console.log(nomeEquipe)
         
@@ -151,40 +93,35 @@ async function buscarPiloto(nomeEquipe) {
         nomeEquipe: []
       } 
 
-      // console.log(data)
-        
-      // for (let p = 0; p < data.length; p++) {
-      //   let pilotos = data[p].team_name
-      //   console.log(pilotos)
-      // }
+      const equipePiloto = {
+        nomePiloto: [],
+        nomeEquipe: []
+      }
 
+      // tamanho do array
+      console.log(data.length);
 
-     // let pilotos = data[0].team_name
-     // console.log(pilotos)
+      // Desta forma percorre o array
       
-
       data.forEach(function(item, indice){
 
         console.log(item.team_name)
 
-        item.team_name.forEach(function(itemEquipe){
+        equipePiloto.nomeEquipe = item.team_name
+        equipePiloto.nomePiloto = item.full_name
 
-          equipePioto.nomeEquipe = itemEquipe.team_name
-          equipePioto.nomePiloto = itemEquipe.full_name
 
-          console.log(equipePiloto)
-
-          if(String(listaEncontrados.nomePiloto).includes(String(equipePioto.nomePiloto)))
+        if(String(listaEncontrados.nomePiloto).includes(String(equipePiloto.nomePiloto))){
 
           listaEncontrados.nomePiloto.push(equipePioto.nomePiloto)
           listaEncontrados.nomeEquipe.push(equipePioto.nomeEquipe)
 
-        })
+        }
       })
+
 
       console.log(listaEncontrados)
 
-      data.for
 
       if (String(nomeEquipe).toLowerCase == String(listaEncontrados.nomeEquipe).toLocaleLowerCase){
 
@@ -210,40 +147,11 @@ async function buscarPiloto(nomeEquipe) {
     return pilotoEncontrado
   
 
+
 }
 
 
 
-
-//Função para obter a foto do piloto na API OPEN F1
-/*async function obterFotoPiloto(numeroPilotoEcontrado) {
-
-  const url = `https://api.openf1.org/v1/drivers?driver_number=${numeroPilotoEcontrado}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  const pilotoEncontrado = {
-    foto: [],
-    nome: []
-  }
-    
-
-
-
-  if (data.data && data.data.length > 0) {
-
-    dataDesc.forEach(function(item){
-      pilotoEncontrado.foto.push(item['headshot_url'])
-      pilotoEncontrado.nome.push(item['full_name'])
-    });
-
-
-  }else{
-    pilotoEncontrado.foto  = './User Male.png'
-    pilotoEncontrado.name = 'Nome'
-  }
-
-  return pilotoEncontrado
-}*/
 
 // Função para criar um card de piloto
 function criarCardPiloto(pilotoEncontrado) {
@@ -290,18 +198,12 @@ function criarCardPiloto(pilotoEncontrado) {
 
 }
 
-async function telaDadosPilotos(){
-  
-  
-
-}
 
 telaInicial()
 
-telaHome()
+// telaHome()
 
-buscarPiloto()
+//buscarPiloto()
 
-pilotoEncontrado.forEach(criarCardPiloto)
-
+// pilotoEncontrado.forEach(criarCardPiloto)
 
